d34ca40573413d2fd868682c323ff66dc5ad2aaa
leeyaonan <yaonan.li@pingcl.com>
2023-05-16 16:56:28 +0800

    init project

---
+++ src/test/java/com/leeyaonan/blog/icefloe/IcefloeBlogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeyaonan.blog.icefloe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcefloeBlogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
